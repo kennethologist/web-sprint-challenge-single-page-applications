6319bafef5a921fbd5925fcd17df516512d543a2
Ken Smith <kennethologist@gmail.com>
2022-02-25 13:54:47 -0500

    Added links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link, Route, Switch } from "react-router-dom";
+import PizzaForm from "./Components/PizzaForm";
 
 const App = () => {
   return (
@@ -7,15 +8,20 @@ const App = () => {
         <header>
           <nav>
             <Link to="/">Home</Link>
-            <Link to="/pizza">Order Pizza</Link>
+            <Link to="/pizza" id="order-pizza">Order Pizza</Link>
           </nav>
 
-          <h1>Pizza Baby!</h1>
-          <p>I'm a Pizza Fanatic so I built a Pizza ordering App!</p>
+          
         </header>
         <Switch>
           <Route exact path ="/">
+            <h1>Pizza Baby!</h1>
+            <p>I'm a Pizza Fanatic so I built a Pizza ordering App!</p>
             <p>Start ya Pizza Order</p>
+          </Route>
+
+          <Route path="/pizza/:id">
+            <PizzaForm></PizzaForm>
           </Route>
         </Switch>
     </div>
--- src/Components/PizzaForm.js
+++ src/Components/PizzaForm.js
@@ -1,9 +1,16 @@
+import { useParams } from "react-router-dom"
 
 function PizzaForm() {
-
+    console.log(useParams())
     return (
         <div>
-            
+            <h1>Order my Pizza</h1>
+
+            <form id="pizza-form">
+
+            </form>
         </div>
     )
 }
+
+export default PizzaForm
