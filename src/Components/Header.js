47517118f510715141d52206978dad19f447fe12
Ken Smith <kennethologist@gmail.com>
2022-03-01 10:26:00 -0500

    Renamed Components

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, NavLink } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 
 function Header() {
